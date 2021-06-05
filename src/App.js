b39e25c485d269bd591dad0c5ede4112d2362213
heredia lucas <heredialucasfac22@hotmail.com>
2021-06-05 20:52:12 -0300

    Haciendo contador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  const [contador, upgradeContador] = useState(0);
 
-      </header>
+  return (
+    <div className="App gap-2">
+      <h1 className="App-Titulo">CONTADOR</h1>
+      <div className="App-Circle">
+        <span className="App-Contador">{contador}</span>
+      </div>
+      <div className="d-grid gap-2">
+        <Button onClick={() => upgradeContador(contador + 1)} variant="primary">
+          Aumentar
+        </Button>
+        <Button
+          onClick={(e) => {
+            contador > 0 ? upgradeContador(contador - 1) : e.preventDefault();
+          }}
+          variant="warning"
+        >
+          Disminuir
+        </Button>
+        <Button onClick={() => upgradeContador(0)} variant="danger">
+          Resetear
+        </Button>
+      </div>
     </div>
   );
 }
